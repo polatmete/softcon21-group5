6103bea8bf09c39b238c04247675374b0cad902a
jemaie <jerome.maier@uzh.ch>
2021-09-26 17:55:56 +0200

    ersti versuech

--- src/ch/uzh/softcon/zero/Main.java
+++ src/ch/uzh/softcon/zero/Main.java
@@ -1,7 +1,20 @@
 package ch.uzh.softcon.zero;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // TODO: Implement
+    public static void main(String[] args){
+        tdoc("", 0);
+    }
+
+    public static void tdoc(String newText, int day){
+        String prologue = String.format("On the %d. day of Christmas\n" +
+                "My true love sent to me:", day);
+        System.out.println(prologue + newText);
+
+        Scanner scan = new Scanner(System.in);
+        String newLine = "\n" + scan.nextLine();
+
+        tdoc(newLine + newText, day + 1);
     }
 }
