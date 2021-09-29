0ffdcf71aeb2d2a492234f97b3ca403c97497457
Elvio Petillo <elvio.petillo@nanowaris.com>
2021-09-29 15:13:18 +0200

    move a0 into subfolder v2

--- a0/src/ch/uzh/softcon/zero/Main.java
+++ a0/src/ch/uzh/softcon/zero/Main.java
@@ -1,19 +1,31 @@
 package ch.uzh.softcon.zero;
 
+import java.util.Scanner;
+
 /**
  * Contributed to by the authors:
  * Elvio Petillo
- * [Insert name]
- * [Insert name]
- * [Insert name]
+ * Jerome Maier
+ * Alessandro Stefanelli
+ * Mete Polat
  */
 
 public class Main {
-    public static void main(String[] args) {
-        // TODO: Implement
+    public static void main(String[] args){
+        tdoc("", 1);
     }
 
-    private static void print_recursively(String previous, String count_str) {
-        // TODO: Implement
+    public static void tdoc(String newText, int day){
+        if (day > 12) {
+            return;
+        }
+        String prologue = String.format("On the %d. day of Christmas\n" +
+                "My true love sent to me:", day);
+        System.out.println(prologue + newText);
+
+        Scanner scan = new Scanner(System.in);
+        String newLine = scan.nextLine();
+
+        tdoc(newLine + newText, day + 1);
     }
 }
