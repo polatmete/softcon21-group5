f0f72db30505870b8afdd4a1f8e9739233478e0f
Elvio Petillo <elvio.petillo@nanowaris.com>
2021-09-29 14:56:10 +0200

    move a0 into subfolder

---
+++ a0/src/ch/uzh/softcon/zero/Main.java
@@ -0,0 +1,19 @@
+package ch.uzh.softcon.zero;
+
+/**
+ * Contributed to by the authors:
+ * Elvio Petillo
+ * [Insert name]
+ * [Insert name]
+ * [Insert name]
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Implement
+    }
+
+    private static void print_recursively(String previous, String count_str) {
+        // TODO: Implement
+    }
+}
