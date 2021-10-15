4c4ff0aa17bfb356e65b292739425cbc1ce265bc
Elvio Petillo <elvio.petillo@nanowaris.com>
2021-10-16 00:57:42 +0200

    Implement isPieceStuck and modify stuff.

--- a1/src/ch/uzh/softcon/one/Turn.java
+++ a1/src/ch/uzh/softcon/one/Turn.java
@@ -9,8 +9,10 @@ public class Turn {
     protected TilePosition from;
     protected TilePosition to;
     public Player activePlayer;
+    public Status status;
 
     public enum Status {
+        PENDING,
         COMPLETED,
         SYNTAX_ERROR,
         ILLEGAL_TURN,
@@ -21,6 +23,7 @@ public class Turn {
     /**
      * Class constructor for the turn class.
      * A single turn consisting of two tile positions.
+     * Status defaults to Status.PENDING.
      * @param x_from FROM x axis
      * @param y_from FROM y axis
      * @param x_to TO x axis
@@ -28,9 +31,24 @@ public class Turn {
      * @param p Active player
      */
     public Turn(int x_from, int y_from, int x_to, int y_to, Player p) {
+        this(x_from, y_from, x_to, y_to, p, Status.PENDING);
+    }
+
+    /**
+     * Class constructor for the turn class.
+     * A single turn consisting of two tile positions.
+     * @param x_from FROM x axis
+     * @param y_from FROM y axis
+     * @param x_to TO x axis
+     * @param y_to TO y axis
+     * @param p Active player
+     * @param s Status
+     */
+    public Turn(int x_from, int y_from, int x_to, int y_to, Player p, Status s) {
         from = new TilePosition(x_from, y_from);
         to = new TilePosition(x_to, y_to);
         activePlayer = p;
+        status = s;
     }
 
     /**
@@ -39,10 +57,11 @@ public class Turn {
      * @param pos x_from, y_from, x_to, y_to
      * @param p Active player
      */
-    public Turn(int[] pos, Player p) {
+    public Turn(int[] pos, Player p, Status s) {
         from = new TilePosition(pos[0], pos[1]);
         to = new TilePosition(pos[2], pos[3]);
         activePlayer = p;
+        status = s;
     }
 
     /**
--- a1/src/ch/uzh/softcon/one/TurnHandler.java
+++ a1/src/ch/uzh/softcon/one/TurnHandler.java
@@ -12,10 +12,12 @@ public class TurnHandler {
         if (checkTransformNeeded(turn)) {
             Board.getPiece(turn.to).setKing();
         }
-
+        // isPieceStuck()
+        // isJumpRequired()
+        // setTurnStatus()
     }
 
-    public static boolean checkWin(Turn turn) {
+    private static boolean checkWin(Turn turn) {
         // TODO: also check if p cannot move? -> lose
         if (turn.activePlayer == Player.RED) {
             return Board.pieceCountWhite == 0;
@@ -26,17 +28,49 @@ public class TurnHandler {
 
     // check if the player is forced to move somewhere and cannot just move anywhere
     // -> Turn object only for player needed
-    public static boolean checkJumpRequired(Turn turn) {
+    private static boolean isJumpRequired(Turn turn) {
         // TODO
         return false;
     }
 
-    public static boolean checkMovePossible(Turn turn) {
+    private static boolean isJumpPossible(int posX, int posY) {
         // TODO
         return false;
     }
 
-    public static boolean checkTransformNeeded(Turn turn) {
+    // TODO: rename on UML from checkMovePossible
+    // This must come before isPieceStuck() and isJumpRequired()
+    /**
+     * it is not stuck if:
+     *  - it can move in any of its two or four diagonal directions
+     *  - it can capture any enemy pieces on the two or four diagonally adjacent tiles
+     */
+    private static boolean isPieceStuck(Turn turn) {
+        int x = turn.status == Turn.Status.COMPLETED ? turn.to.x() : turn.from.x();
+        int y = turn.status == Turn.Status.COMPLETED ? turn.to.y() : turn.from.y();
+
+        // TODO: From where does the red player start?
+        if (turn.activePlayer == Player.RED || Board.getPiece(turn.from).isKing) {
+            if (x > 1 && y < 8) {
+                if (Board.getPiece(x - 1, y + 1) == null) return false;
+            }
+            if (x < 8 && y < 8) {
+                if (Board.getPiece(x + 1, y + 1) == null) return false;
+            }
+        }
+
+        if (turn.activePlayer == Player.WHITE || Board.getPiece(turn.from).isKing) {
+            if (x > 1) {
+                if (Board.getPiece(x - 1, y - 1) == null) return false;
+            } else {
+                if (Board.getPiece(x + 1, y - 1) == null) return false;
+            }
+        }
+
+        return !isJumpPossible(x, y);
+    }
+
+    private static boolean checkTransformNeeded(Turn turn) {
         if (turn.activePlayer == Player.RED) {
             return turn.to.y() == 0;
         } else {
@@ -44,11 +78,14 @@ public class TurnHandler {
         }
     }
 
-    public static void executeCapture(Turn turn) {
+    private static void executeCapture(Turn turn) {
         int enemyX = (turn.from.x() + turn.to.x()) / 2;
         int enemyY = (turn.from.y() + turn.to.y()) / 2;
         Board.removePiece(enemyX, enemyY);
         Board.movePiece(turn);
     }
 
+    private static Turn setTurnStatus(Turn turn) {
+        // TODO: Implement and add to UML!
+    }
 }
