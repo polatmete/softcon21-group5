bd3e7498e3d6d00005964db0c2a7fa974e075b53
Mete Polat <mail@metepolat.ch>
2021-10-17 01:18:39 +0200

    IOFormatter: Finished formatOutput

--- a1/src/ch/uzh/softcon/one/IOFormatter.java
+++ a1/src/ch/uzh/softcon/one/IOFormatter.java
@@ -7,7 +7,36 @@ public class IOFormatter {
     }
 
     public static String formatOutput(String textBeforeBoard, boolean printBoard, String textAfterBoard) {
-        // TODO
-        return null;
+        String out = "";
+        if (!textBeforeBoard.equals("")) out += textBeforeBoard + "\n";
+
+        if (printBoard) {
+            out += "      a     b     c     d     e     f     g     h\n" +
+                    "  +-------------------------------------------------+\n";
+            for (int i = 0; i < 8; ++i) {
+                String tmp = 8-i + " | ";
+                for (int j = 0; j < 8; ++j) {
+                    Piece piece = Board.getPiece(i, j);
+                    if (piece == null) tmp += "[   ] ";
+                    else {
+                        String color = "";
+                        String type = "";
+
+                        if (piece.color == Player.RED) color = "R";
+                        else if (piece.color == Player.WHITE) color = "W";
+                        if (piece.isKing) type = "K";
+                        else type = "P";
+
+                        tmp += "[" + color + "_" + type + "] ";
+                    }
+                }
+                tmp += "| " + (8-i) + "\n";
+                out += tmp;
+            }
+            out +=  "  +-------------------------------------------------+\n" +
+                    "      a     b     c     d     e     f     g     h\n";
+        }
+        out += textAfterBoard;
+        return out;
     }
 }
