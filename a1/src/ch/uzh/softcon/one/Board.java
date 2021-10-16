1ce66ef5e3ed286a1e5117ff7c175cdb7db37b23
alstefa <91147667+alstefa@users.noreply.github.com>
2021-10-16 19:46:08 +0200

    added Board and Piece class

---
+++ a1/src/ch/uzh/softcon/one/Board.java
@@ -0,0 +1,65 @@
+package ch.uzh.softcon.one;
+
+public class Board {
+    public Piece[][] positions;
+    public int pieceCountRed;
+    public int pieceCountWhite;
+
+    public void initialize() {
+
+        this.positions = new Piece[8][8];
+
+        int[][][] initialPosRed = {
+                {{0}, {1, 3, 5, 7}},
+                {{1}, {0, 2, 4, 6}},
+                {{2}, {1, 3, 5, 7}}
+        };
+
+        int[][][] initialPosWhite = {
+                {{5}, {0, 2, 4, 6}},
+                {{6}, {1, 3, 5, 7}},
+                {{7}, {0, 2, 4, 6}},
+        };
+
+        //Put red pieces on the board
+
+        for (int i = 0; i < initialPosRed.length; i++) {
+            Piece newPiece = new Piece();
+            newPiece.color = Player.RED;
+
+            int row = initialPosRed[i][0][0];
+            int[] columns = initialPosRed[i][1];
+
+            for (int column : columns) {
+                this.positions[row][column] = newPiece;
+            }
+        }
+
+        //Put white pieces on the board
+
+        for (int i = 0; i < initialPosWhite.length; i++) {
+            Piece newPiece = new Piece();
+            newPiece.color = Player.WHITE;
+
+            int row = initialPosWhite[i][0][0];
+            int[] columns = initialPosWhite[i][1];
+
+            for (int column : columns) {
+                this.positions[row][column] = newPiece;
+            }
+        }
+    }
+
+    public void movePiece(Turn turn) {
+        this.positions[turn.to.x()][turn.to.y()] = this.positions[turn.from.x()][turn.from.y()];
+        removePiece(turn.from.x(), turn.from.x());
+    }
+
+    public void removePiece(int posX, int posY) {
+        this.positions[posX][posY] = null;
+    }
+
+    public Piece getPiece(int posX, int posY) {
+        return this.positions[posX][posY];
+    }
+}
