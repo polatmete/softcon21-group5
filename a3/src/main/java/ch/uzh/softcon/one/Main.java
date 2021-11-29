ff0a4baee26eda89e3dff18bc73747a3dd759446
Mete Polat <mail@metepolat.ch>
2021-11-29 12:49:25 +0100

    Removed Launcher class. Directly start from Main

--- a3/src/main/java/ch/uzh/softcon/one/Main.java
+++ a3/src/main/java/ch/uzh/softcon/one/Main.java
@@ -1,10 +1,17 @@
-package ch.uzh.softcon.one;
+package ch.uzh.softcon.one.utils;
 
-import ch.uzh.softcon.one.utils.Launcher;
+import ch.uzh.softcon.one.abstraction.GameHandling;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) {
-        Launcher.main(args);
+    public static void main(String[] args) { // Setup and initialize game
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        GameHandling.initialize(stage);
     }
 }
