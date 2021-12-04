2c11a5dde58ff6cb27f7395fbc2a1c45e85f6edd
jemai <jerome.maier@uzh.ch>
2021-12-04 12:43:43 +0100

    feat(BJ-24): cleanups, fixes, further implementations, added logic files

--- a4/src/main/java/ch/uzh/softcon/four/logic/Game.java
+++ a4/src/main/java/ch/uzh/softcon/four/logic/Game.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Game {
-    
+
     Map<String, Integer> bets = new HashMap<>();
 
     public static void distributeCards(Player[] players) {
