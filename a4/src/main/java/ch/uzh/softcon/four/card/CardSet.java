75f831dfacf3d1a6e46f501297193cfb4feb5b43
jemai <jerome.maier@uzh.ch>
2021-12-05 17:55:15 +0100

    feat(BJ-24): cleanups, changes, fixes

--- a4/src/main/java/ch/uzh/softcon/four/card/CardSet.java
+++ a4/src/main/java/ch/uzh/softcon/four/card/CardSet.java
@@ -3,26 +3,23 @@ package ch.uzh.softcon.four.card;
 import ch.uzh.softcon.four.card.Card.Suit;
 import ch.uzh.softcon.four.card.Card.Rank;
 
-import java.util.Arrays;
-
 public class CardSet {
 
-    private Card[] cards;
+    private final Card[] cards;
 
     public CardSet() {
         this.cards = new Card[52];
         int i = 0;
         for (Suit s : Suit.values()) {
             for (Rank r : Rank.values()) {
-                this.cards[i++] = new Card(s, r);
+                this.cards[i] = new Card(s, r);
+                i++;
             }
         }
     }
 
     public Card[] drawCards() {
-        Card[] drawnCards = Arrays.copyOf(this.cards, this.cards.length);
-        this.cards = null;
-        return drawnCards;
+        return this.cards;
     }
 
     public int size() {
