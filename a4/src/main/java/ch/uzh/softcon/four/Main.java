5ac8d3a7d6fa60fa308abdc3dc89bb3161f53b6f
Mete Polat <mail@metepolat.ch>
2021-12-15 03:56:56 +0100

    Fine tuning + comments added

--- a4/src/main/java/ch/uzh/softcon/four/Main.java
+++ a4/src/main/java/ch/uzh/softcon/four/Main.java
@@ -1,30 +1,31 @@
 package ch.uzh.softcon.four;
 
 import ch.uzh.softcon.four.logic.Game;
+//TODO import ch.uzh.softcon.four.scoreboard.ScoreBoard;
 
 public class Main {
     public static void main(String[] args) {
-        Game.initialize();
-        System.out.println("\n");
+        Game.initialize(); // Set difficulty and players, initialize some variables
+        System.out.println(); // New line for better design
         while (true) {
-            Game.startNewRound(); // First give dealer 2 cards
+            Game.startNewRound(); // First, give dealer 2 cards
 
             boolean quitGame = false;
             for (int i = 0; i < 5; ++i) {
-                Game.takeBets(i);
+                Game.takeBets(i); // Ask player for bet or to leave every time a new round begins
 
-                quitGame = !Game.checkPlayerLeft();
+                quitGame = !Game.checkPlayerLeft(); // Check whether a player is left after takeBets() since players can only leave at this point
                 if(quitGame) break; // If all players left end game
 
-                Game.play(i, 0);
+                Game.play(i, 0); // Play with every player
             }
-            if (quitGame) break;
+            if (quitGame) break; // If all players left end game
 
-            Game.playDealer();
-            Game.evaluate();
-            Game.conclude();
+            Game.playDealer(); // At the end dealer should draw cards
+            Game.evaluate(); // Check for each player and hand who has won
+            Game.conclude(); // Clear hands, kick out players without money, allow new players to join
         }
-        System.out.println("\nIt seems like no one wants to play at this table any more.. Tank you for joining and bye for now ;)");
-        // TODO Print ScoreBoard
+        System.out.print("\nIt seems like no one wants to play at this table any more.. Tank you for joining and bye for now ;)");
+        // TODO ScoreBoard.printScore();
     }
 }
